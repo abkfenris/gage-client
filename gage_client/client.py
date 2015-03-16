b025758103092f2c2bf7281e4f4b4afb3ea1ea65
Alex Kerney <abk@mac.com>
2015-03-15 22:49:48 -0400

    Getting some logging in there, and having sender_ids communicated with the server, at least with the 0.1 version of the API, and clearing out successfully added samples

--- gage_client/client.py
+++ gage_client/client.py
@@ -1,5 +1,6 @@
 from itsdangerous import JSONWebSignatureSerializer
 import requests
+import logging
 
 class AuthenticationError(Exception):
     """
@@ -18,7 +19,9 @@ class Client(object):
         Builds the right client class for the API based on the url given
         """
         if '/0.1' in url:
+            logging.debug('Creating Client_0_1 for API version 0.1')
             return Client_0_1.__new__(Client_0_1, url, id, password)
+        logging.error('Did not create Client based on url')
 
     def __init__(self, url, id, password):
         """
@@ -29,15 +32,18 @@ class Client(object):
             id (int): Client ID number
             password (str): Password string to submit samples
         """
+        logging.error('__init__ not implemented on generic Client class')
         raise NotImplementedError
 
     def reading(self, sensor, dt, value):
         """
         Add a new reading to the ones the Client will send to the server
         """
+        logging.error('reading method not implemented on generic Client class')
         raise NotImplementedError
 
     def send_all(self):
+        logging.error('send_all method not implemented on generic Client class')
         raise NotImplementedError
 
 
@@ -59,8 +65,9 @@ class Client_0_1(Client):
         self.id = id
         self.serializer = JSONWebSignatureSerializer(password)
         self.samples = []
+        logging.debug('Client_0_1 initialized for {url} and gage id {id}'.format(url=url, id=id))
 
-    def reading(self, sensor, dt, value):
+    def reading(self, sensor, dt, value, id=None):
         """
         Add a new reading to send at next connection
 
@@ -68,10 +75,18 @@ class Client_0_1(Client):
             sensor (str): Type of sensor (level, current, voltage)
             dt (datetime string): Datetime string of sensor reading
             value (float): Float value of sensor reading
+            id (int): Integer identifying sample, if not given, just use length of sample list
         """
-        self.samples.append({'type': sensor,
+        if id is None:
+            self.samples.append({'type': sensor,
                                  'datetime': dt,
-                                 'value': value})
+                                 'value': value,
+                                 'sender_id': len(self.samples)})
+        else:
+            self.samples.append({'type': sensor,
+                                 'datetime': dt,
+                                 'value': value,
+                                 'sender_id': id})
 
     def send_all(self):
         """
@@ -90,5 +105,15 @@ class Client_0_1(Client):
         if r.status_code is 401:
             raise AuthenticationError
         elif r.status_code is 200 and r.json()['result'] == 'created':
-            return (True, r.json()['samples'])
+            sucessful_ids = []
+            for sample in r.json()['samples']:
+                sucessful_ids.append(sample['sender_id'])
+            print sucessful_ids
+            samples = self.samples
+            for x in range(len(samples)):
+                if samples[x]['sender_id'] in sucessful_ids:
+                    self.samples.pop(x)
+            print self.samples
+            return (True, sucessful_ids)
+
         return False
